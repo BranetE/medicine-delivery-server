22378a782b191f7393a35e66579e49c9cec2b3dd
Oleh Kulbaba <elatedexorcist@gmail.com>
2024-03-13 14:42:00 +0200

    Completed orderController

--- src/controllers/orderController.ts
+++ src/controllers/orderController.ts
@@ -1,13 +1,39 @@
 import express from "express";
 import { createOrder } from "../database/order";
+import { ProductModel, getById } from "../database/product";
+
+type User = {
+  name: string;
+  email: string;
+  phone: string;
+  address: string;
+};
+
+type Product = {
+  product: string;
+  quantity: number;
+  totalPrice?: number;
+};
+
+interface ReqBody {
+  user: User;
+  products: Product[];
+}
 
 export const createNewOrder = async (
   req: express.Request,
   res: express.Response
 ) => {
   try {
-    const { user, products } = req.body;
-    const order = await createOrder({ user, products });
+    const order: ReqBody = req.body;
+    order.products = await Promise.all(
+      order.products.map(async (e) => {
+        var price = (await getById(e.product).exec()).price;
+        e.totalPrice = price * e.quantity;
+        return e;
+      })
+    );
+    await createOrder(order);
     return res.sendStatus(200);
   } catch (error) {
     console.log(error);
